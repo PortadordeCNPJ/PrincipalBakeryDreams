2bf7c31d44950d722bd58041840d0d87d27d986c
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-03-31 00:54:38 -0300

    Add: atualização do banco de teste

--- public/assets/sql/curso_pdo.sql
+++ public/assets/sql/curso_pdo.sql
@@ -1,15 +1,122 @@
-CREATE TABLE users (
-    id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL, 
-    email VARCHAR(50) UNIQUE KEY NOT NULL,
-    password VARCHAR(158) NULL,
-    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 31/03/2024 às 05:52
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.2.4
 
-CREATE TABLE posts (
-    id INTEGER(11) PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL, 
-    user INTEGER(11) NOT NULL,
-    description TEXT NOT NULL,
-    created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `curso_pdo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `user` int(11) NOT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_produtos`
+--
+
+CREATE TABLE `tb_produtos` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `price` float NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_produtos`
+--
+
+INSERT INTO `tb_produtos` (`id`, `name`, `price`, `description`, `image`) VALUES
+(1, 'Bolo Chocolate', 45.65, 'Bolo de chocolate ao leite quente', './assets/img/chocolate.jpg'),
+(2, 'Bola de Morango', 35.99, 'Tem morango', './assets/img/morangobolo.jpg'),
+(3, 'Bolo de Cenoura Africana', 30.99, 'Tem cenoura e tem Africano', './assets/img/bolocenoura.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(150) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+(1, 'Guilherme', 'guilhermedesouzamuller@hotmail.com', '753951', '2024-01-16 14:42:09'),
+(2, 'João do Carma', 'joao@gmail.com', 'jaoao', '2024-01-16 16:45:19'),
+(3, 'Maria', 'mariaKratos@hotmail.com', 'mariaKratos', '2024-01-16 16:45:19'),
+(4, 'guilherme de souza ', 'teste@teste.com', '123', '2024-02-19 23:18:59'),
+(6, 'guilherme de souza ', 'teste1@teste.com', '123', '2024-02-19 23:30:11');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices de tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
