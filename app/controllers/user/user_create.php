3cfa2226c6ee6a36db979b09ef19c7f0b3addecb
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-05-15 14:48:42 -0300

    Add: Css e tela de cadastro (falta a de login para fazer)

--- app/controllers/user/user_create.php
+++ app/controllers/user/user_create.php
@@ -1,5 +1,3 @@
 <?php
-
-echo "<script>alert('Cadastro realizado com sucesso');</script>";
 //Direciona para a página de user_create
 $layout->add('user_create');
--- app/views/user_create.php
+++ app/views/user_create.php
@@ -1,22 +1,40 @@
 <h2>Cadastrar User</h2>
+    <img class="img-for" src="./assets/img/cake.png">
 
-<form action="/user_store" method="POST" role="form">
+    <form id="form" class="colorful-form" method="post" action="/user_store">
+        <input type="hidden" name="acao" value="cadastrar">
+        <div class="form-group">
+            <label class="form-label" for="name">Name:</label>
+            <input required="" placeholder="Insira seu nome" class="form-input" name="nome" id="name" type="text">
+        </div>
 
-    <div class="form-group">
-        <label for="">Nome</label>
-        <input type="text" class="form-control" name="name" placeholder="Digite seu nome">
-    </div>
+        <div class="form-group">
+            <label class="form-label" for="email">Email:</label>
+            <input required="" placeholder="Insira seu email" class="form-input" name="email" id="email" type="email">
+        </div>
 
-    <div class="form-group">
-        <label for="">Email</label>
-        <input type="text" class="form-control" name="email" placeholder="Digite seu email">
-    </div>
+        <div class="form-group">
+            <label class="form-label" for="name">CPF:</label>
+            <input required="" placeholder="Insira seu CPF" class="form-input" maxlength="11" name="cpf" id="CPF" type="number">
+        </div>
 
-    <div class="form-group">
-        <label for="">Password</label>
-        <input type="password" class="form-control" name="password" placeholder="Digite sua senha">
-    </div>
+        <div class="form-group">
+            <label class="form-label" for="name">Senha:</label>
+            <div class="inputwrapper">
+                <input required="" placeholder="Senha" class="form-input" name="senha" id="senha" type="password" onchange="password()">
+                <button class="eyebt" type="button" id="eye">
+                    <img src="https://cdn0.iconfinder.com/data/icons/feather/96/eye-16.png" alt="eye" />
+                </button>
+            </div>
+        </div>
 
+        <div class="form-group">
+            <label class="form-label" for="name">Confirme sua senha: </label>
+            <input required="" placeholder="Confirme sua senha" class="form-input" name="pwd" id="pwd" type="password" onchange="password()">
+            <div class="error" id="erro-senha">Senhas não coincidem </div>
+        </div>
 
-    <button type="submit" class="btn btn-primary">cadastrar</button>
-</form>
+        <!-- <button onclick="logar()" class="form-button" type="submit">Cadastrar</button> -->
+    </form>
+
+<script src="assets/js/scriptcad.js"></script>
