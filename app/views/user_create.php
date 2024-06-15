232edebba4b35faa143d1232fd8b54f7ea5010e2
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-06-15 14:09:00 -0300

    Add: Arramando o cadastro de cliente e adicionando o arquivo js da página

--- app/views/user_create.php
+++ app/views/user_create.php
@@ -1,39 +1,38 @@
 <h2>Cadastrar User</h2>
-    <img class="img-for" src="./assets/img/cake.png">
+<img class="img-for" src="./assets/img/cake.png">
 
-    <form id="form" class="colorful-form" method="post" action="/user_store">
-        <div class="form-group">
-            <label class="form-label" for="name">Name:</label>
-            <input required="" placeholder="Insira seu nome" class="form-input" name="nome" id="name" type="text">
+<form id="form" class="colorful-form" method="post" action="/user_store">
+    <div class="form-group">
+        <label class="form-label" for="name">Name:</label>
+        <input required="" placeholder="Insira seu nome" class="form-input" name="nome" id="name" type="text">
+    </div>
+
+    <div class="form-group">
+        <label class="form-label" for="email">Email:</label>
+        <input required="" placeholder="Insira seu email" class="form-input" name="email" id="email" type="email">
+    </div>
+
+    <div class="form-group">
+        <label class="form-label" for="name">CPF:</label>
+        <input required="" placeholder="Insira seu CPF" class="form-input" maxlength="11" name="cpf" id="CPF" type="number">
+    </div>
+
+    <div class="form-group">
+        <label class="form-label" for="name">Senha:</label>
+        <div class="inputwrapper">
+            <input required="" placeholder="Senha" class="form-input" name="senha" id="senha" type="password" onchange="password()">
+            <button class="eyebt" type="button" id="eye">
+                <img src="https://cdn0.iconfinder.com/data/icons/feather/96/eye-16.png" alt="eye" />
+            </button>
         </div>
+    </div>
 
-        <div class="form-group">
-            <label class="form-label" for="email">Email:</label>
-            <input required="" placeholder="Insira seu email" class="form-input" name="email" id="email" type="email">
-        </div>
+    <div class="form-group">
+        <label class="form-label" for="name">Confirme sua senha: </label>
+        <input required="" placeholder="Confirme sua senha" class="form-input" name="pwd" id="pwd" type="password" onchange="password()">
+        <div class="error" id="erro-senha">Senhas não coincidem </div>
+    </div>
+    <button onclick="logar()" class="form-button" type="submit">Cadastrar</button>
+</form>
 
-        <div class="form-group">
-            <label class="form-label" for="name">CPF:</label>
-            <input required="" placeholder="Insira seu CPF" class="form-input" maxlength="11" name="cpf" id="CPF" type="number">
-        </div>
-
-        <div class="form-group">
-            <label class="form-label" for="name">Senha:</label>
-            <div class="inputwrapper">
-                <input required="" placeholder="Senha" class="form-input" name="senha" id="senha" type="password" onchange="password()">
-                <button class="eyebt" type="button" id="eye">
-                    <img src="https://cdn0.iconfinder.com/data/icons/feather/96/eye-16.png" alt="eye" />
-                </button>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label class="form-label" for="name">Confirme sua senha: </label>
-            <input required="" placeholder="Confirme sua senha" class="form-input" name="pwd" id="pwd" type="password" onchange="password()">
-            <div class="error" id="erro-senha">Senhas não coincidem </div>
-        </div>
-
-        <!-- <button onclick="logar()" class="form-button" type="submit">Cadastrar</button> -->
-    </form>
-
-<script src="assets/js/scriptcad.js"></script>
+<script src="../../public/assets/js/scriptcad.js"></script>
---
+++ public/assets/js/scriptcad.js
@@ -0,0 +1,34 @@
+//Script para mostrar senha caso o usuario queira ve-lá
+
+function show() {
+    var b = document.getElementById('senha');
+    b.setAttribute('type', 'text');
+}
+
+function hide() {
+    var b = document.getElementById('senha');
+    b.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false); 
+
+function password() {
+    const senha = document.querySelector('input[name=senha]');
+    const confSenha = document.querySelector('input[name=pwd]');
+
+    if (confSenha.value === senha.value) {
+        document.getElementById("erro-senha").style.display = "none";
+    } else {
+        document.getElementById("erro-senha").style.display = "block";
+    }
+}
