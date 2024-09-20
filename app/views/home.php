c4ec1ec2c3ab06ab9eb27b5de63a1d924e5f7564
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-09-20 08:18:53 -0300

    Add: Página inicial com slick slide, agora sim vai ficar bonito

--- app/views/home.php
+++ app/views/home.php
@@ -7,8 +7,64 @@
             <div class="containerProds">
                 <!-- Esse código intera na variavel de produtos, mostrando os valores que ela recebe, também transformando $product como
                  a variável que vai receber o valor da variavel $products -->
+                <!-- <div class="center"> -->
                 <?php foreach ($products as $index => $product) : ?>
                     <?php if ($product->id_tipoproduto == 1) : ?>
+                        <div class="cardProds">
+                            <div class="productsInsideCart">
+                                <img src="../assets/images/<?php echo $product->imagem; ?>" class="imgsCardProduct" alt="">
+                            </div>
+                            <div class="textProduct">
+                                <p>
+                                    <?php echo $product->nome; ?>
+                                </p>
+                                <p>
+                                    <?php echo $product->descricao; ?>
+                                </p>
+                                <p>
+                                    <span>R$<?php echo number_format($product->valor, 2, ',', '.'); ?></span>
+                                    <a href="/cart_add?id_produto=<?php echo $product->id_produto ?>"> Adicionar <i class='fa-solid fa-cart-shopping'></i></a>
+                                </p>
+                            </div>
+                        </div>
+                    <?php endif ?>
+                <?php endforeach ?>
+                <!-- </div> -->
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="containerProds">
+                <!-- Esse código intera na variavel de produtos, mostrando os valores que ela recebe, também transformando $product como
+                 a variável que vai receber o valor da variavel $products -->
+                <?php foreach ($products as $index => $product) : ?>
+                    <?php if ($product->id_tipoproduto == 2) : ?>
+                        <div class="cardProds">
+                            <div class="productsInsideCart">
+                                <img src="../assets/images/<?php echo $product->imagem; ?>" class="imgsCardProduct" alt="">
+                            </div>
+                            <div class="textProduct">
+                                <p>
+                                    <?php echo $product->nome; ?>
+                                </p>
+                                <p>
+                                    <?php echo $product->descricao; ?>
+                                </p>
+                                <p>
+                                    <span>R$<?php echo number_format($product->valor, 2, ',', '.'); ?></span>
+                                    <a href="/cart_add?id_produto=<?php echo $product->id_produto ?>"> Adicionar <i class='fa-solid fa-cart-shopping'></i></a>
+                                </p>
+                            </div>
+                        </div>
+                    <?php endif ?>
+                <?php endforeach ?>
+            </div>
+        </div>
+        <div class="row mb-5">
+            <div class="containerProds">
+                <!-- Esse código intera na variavel de produtos, mostrando os valores que ela recebe, também transformando $product como
+                 a variável que vai receber o valor da variavel $products -->
+                <?php foreach ($products as $index => $product) : ?>
+                    <?php if ($product->id_tipoproduto == 3) : ?>
                         <div class="cardProds">
                             <div class="productsInsideCart">
                                 <img src="../assets/images/<?php echo $product->imagem; ?>" class="imgsCardProduct" alt="">
@@ -31,61 +87,34 @@
             </div>
         </div>
     </div>
-    <div class="row mb-5">
-        <div class="containerProds">
-            <!-- Esse código intera na variavel de produtos, mostrando os valores que ela recebe, também transformando $product como
-                 a variável que vai receber o valor da variavel $products -->
-            <?php foreach ($products as $index => $product) : ?>
-                <?php if ($product->id_tipoproduto == 2) : ?>
-                    <div class="cardProds">
-                        <div class="productsInsideCart">
-                            <img src="../assets/images/<?php echo $product->imagem; ?>" class="imgsCardProduct" alt="">
+    <div class="center" data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
+        <?php foreach ($products as $index => $product) : ?>
+            <?php if ($product->id_tipoproduto == 1) : ?>
+                <div>
+                    <h3>
+                        <div class="cardProds">
+                            <div class="productsInsideCart">
+                                <img src="../assets/images/<?php echo $product->imagem; ?>" class="imgsCardProduct" alt="">
+                            </div>
+                            <div class="textProduct">
+                                <p>
+                                    <?php echo $product->nome; ?>
+                                </p>
+                                <p>
+                                    <?php echo $product->descricao; ?>
+                                </p>
+                                <p>
+                                    <span>R$<?php echo number_format($product->valor, 2, ',', '.'); ?></span>
+                                    <a href="/cart_add?id_produto=<?php echo $product->id_produto ?>"> Adicionar <i class='fa-solid fa-cart-shopping'></i></a>
+                                </p>
+                            </div>
                         </div>
-                        <div class="textProduct">
-                            <p>
-                                <?php echo $product->nome; ?>
-                            </p>
-                            <p>
-                                <?php echo $product->descricao; ?>
-                            </p>
-                            <p>
-                                <span>R$<?php echo number_format($product->valor, 2, ',', '.'); ?></span>
-                                <a href="/cart_add?id_produto=<?php echo $product->id_produto ?>"> Adicionar <i class='fa-solid fa-cart-shopping'></i></a>
-                            </p>
-                        </div>
-                    </div>
-                <?php endif ?>
-            <?php endforeach ?>
-        </div>
+                    </h3>
+                </div>
+            <?php endif ?>
+        <?php endforeach ?>
     </div>
-    <div class="row mb-5">
-        <div class="containerProds">
-            <!-- Esse código intera na variavel de produtos, mostrando os valores que ela recebe, também transformando $product como
-                 a variável que vai receber o valor da variavel $products -->
-            <?php foreach ($products as $index => $product) : ?>
-                <?php if ($product->id_tipoproduto == 3) : ?>
-                    <div class="cardProds">
-                        <div class="productsInsideCart">
-                            <img src="../assets/images/<?php echo $product->imagem; ?>" class="imgsCardProduct" alt="">
-                        </div>
-                        <div class="textProduct">
-                            <p>
-                                <?php echo $product->nome; ?>
-                            </p>
-                            <p>
-                                <?php echo $product->descricao; ?>
-                            </p>
-                            <p>
-                                <span>R$<?php echo number_format($product->valor, 2, ',', '.'); ?></span>
-                                <a href="/cart_add?id_produto=<?php echo $product->id_produto ?>"> Adicionar <i class='fa-solid fa-cart-shopping'></i></a>
-                            </p>
-                        </div>
-                    </div>
-                <?php endif ?>
-            <?php endforeach ?>
-        </div>
-    </div>
-    </div>
+
     <script>
         $('.center').slick({
             centerMode: true,
--- app/views/layout.php
+++ app/views/layout.php
@@ -36,6 +36,10 @@ $productsInCart = $cart->cart();
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/fontawesome.min.css" integrity="sha512-UuQ/zJlbMVAw/UU8vVBhnI4op+/tFOpQZVT+FormmIEhRSCnJWyHiBbEVgM4Uztsht41f3FzVWgLuwzUqOObKw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" type="text/css" href="../../assets/css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="../../assets/css/slick-theme.css" />
+
+
     <title>Views</title>
 </head>
 
