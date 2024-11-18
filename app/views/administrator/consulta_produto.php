e978063b542bef55bd801a5bbaa3dfcab764a4c5
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2024-11-18 00:24:25 -0300

    Add: Inativar produtos(terminar)

--- app/views/administrator/consulta_produto.php
+++ app/views/administrator/consulta_produto.php
@@ -2,32 +2,31 @@
     <input type="text" placeholder="Filtro" name="filter_products">
 </form>
 <table class="table table-striped table-hover">
-<thead>
-    <tr>
-        <th>Id</th>
-        <th>Nome</th>
-        <th>Preço</th>
-        <th>Sabor</th>
-        <th>Tipo</th>
-    </tr>
-</thead>
-<tbody>
-    <?php foreach ($produtos as $produto) :
-    ?>
+    <thead>
         <tr>
-            <td><?= $produto->id_produto; ?></td>
-            <td><?= $produto->nome; ?></td>
-            <td><?= $produto->valor; ?></td>
-            <td><?= $produto->sabor; ?></td>
-            <td><?= $produto->tipoproduto; ?></td>
-
-            <td>
-                <a href="/products_edit?id_produto=<?= $produto->id_produto; ?>" class="btn btn-success">Editar</a>
-            </td>
-            <td>
-                <a href="/products_destroy?id_produto=<?= $produto->id_produto; ?>" class="btn btn-danger">Deletar</a>
-            </td>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Sabor</th>
+            <th>Tipo</th>
+            <th>Ações</th>
         </tr>
-    <?php endforeach; ?>
-</tbody>
+    </thead>
+    <tbody>
+        <?php foreach ($produtos as $produto): ?>
+            <!-- Adiciona a classe 'inactive' se o produto estiver inativo -->
+            <tr class="<?= $produto->ativo === 'N' ? 'inactive' : '' ?>">
+                <td><?= $produto->id_produto; ?></td>
+                <td><?= $produto->nome; ?></td>
+                <td><?= $produto->valor; ?></td>
+                <td><?= $produto->sabor; ?></td>
+                <td><?= $produto->tipoproduto; ?></td>
+
+                <td>
+                    <a href="/products_edit?id_produto=<?= $produto->id_produto; ?>" class="btn btn-success">Editar</a>
+                    <a href="/products_destroy?id_produto=<?= $produto->id_produto; ?>" class="btn btn-danger">Deletar</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
 </table>
--- app/views/administrator/products_edit.php
+++ app/views/administrator/products_edit.php
@@ -4,32 +4,30 @@
 
 <form id="form" class="colorful-form" method="post" action="/products_update">
     <input type="hidden" name="id_produto" value="<?= $productsFound->id_produto ?>">
+
     <div class="form-group">
         <label class="form-label" for="name">Nome:</label>
-        <input required="" placeholder="Insira o nome do produto" class="form-input" name="nome" id="name" type="text" value="<?= $productsFound->nome ?>">
+        <input required placeholder="Insira o nome do produto" class="form-input" name="nome" id="name" type="text" value="<?= $productsFound->nome ?>">
     </div>
 
     <div class="form-group">
         <label class="form-label" for="descricao">Descrição do produto:</label>
-        <input required="" placeholder="Insira a descrição do produto" class="form-input" name="descricao" id="email" type="text" value="<?= $productsFound->descricao ?>">
+        <input required placeholder="Insira a descrição do produto" class="form-input" name="descricao" id="descricao" type="text" value="<?= $productsFound->descricao ?>">
     </div>
 
     <div class="form-group">
-        <label class="form-label" for="valor">valor:</label>
-        <input required="" placeholder="Insira o valor do produto" class="form-input" maxlength="11" name="valor" type="text" inputmode="numeric" value="<?= $productsFound->valor ?>">
+        <label class="form-label" for="valor">Valor:</label>
+        <input required placeholder="Insira o valor do produto" class="form-input" maxlength="11" name="valor" type="text" inputmode="numeric" value="<?= $productsFound->valor ?>">
     </div>
 
     <div class="form-group">
-        <label class="form-label" for="imagem">imagem:</label>
-        <img src="../assets/images/<?php echo $productsFound->imagem ?>" alt="" width="300">
-        <input class="form-input" name="imagem" id="senha" type="file">
+        <label class="form-label" for="imagem">Imagem:</label>
+        <img src="../assets/images/<?= $productsFound->imagem ?>" alt="Imagem do produto" width="300">
+        <input class="form-input" name="imagem" id="imagem" type="file">
     </div>
 
-    <!-- Fazer um if que vai verificar o tipo do produto e vai com base nele, cadastrar no campo correto -->
-
     <div class="form-group">
-        <label class="form-label" for="gluten">Tipo do Produto:</label>
-
+        <label class="form-label" for="id_tipoproduto">Tipo do Produto:</label>
         <select name="id_tipoproduto">
             <?php foreach ($kindProducts as $kindProduct) {
                 echo "<option selected value='" . $kindProduct->id_tipoproduto . "'>" . $kindProduct->tipoproduto . "</option>";
@@ -38,28 +36,35 @@
     </div>
 
     <div class="form-group">
-        <label class="form-label" for="gluten">Contem Gluten:</label>
-        <label class="form-label-radio" for="sim">Sim</label> <input checked type="radio" name="gluten" value="S">
-        <label class="form-label-radio" for="nao">Não</label> <input type="radio" name="gluten" value="N">
+        <label class="form-label" for="gluten">Contém Glúten:</label>
+        <label class="form-label-radio" for="gluten-sim">Sim</label>
+        <input checked type="radio" id="gluten-sim" name="gluten" value="S">
+        <label class="form-label-radio" for="gluten-nao">Não</label>
+        <input type="radio" id="gluten-nao" name="gluten" value="N">
     </div>
 
     <div class="form-group">
-        <label class="form-label" for="gluten">Sabor:</label>
-        <input required="" placeholder="Ex: 4 Leites e chocolate" class="form-input" name="sabor" type="text" value="<?= $productsFound->sabor ?>">
+        <label class="form-label" for="sabor">Sabor:</label>
+        <input required placeholder="Ex: 4 Leites e chocolate" class="form-input" name="sabor" type="text" value="<?= $productsFound->sabor ?>">
     </div>
 
     <div class="form-group">
-        <label class="form-label" for="amendoim">Contem Amendoim:</label>
-        <label class="form-label-radio" for="sim">Sim</label> <input type="radio" name="amendoim" value="S">
-        <label class="form-label-radio" for="nao">Não</label> <input checked type="radio" name="amendoim" value="N">
+        <label class="form-label" for="amendoim">Contém Amendoim:</label>
+        <label class="form-label-radio" for="amendoim-sim">Sim</label>
+        <input type="radio" id="amendoim-sim" name="amendoim" value="S">
+        <label class="form-label-radio" for="amendoim-nao">Não</label>
+        <input checked type="radio" id="amendoim-nao" name="amendoim" value="N">
     </div>
 
     <div class="form-group">
-        <label class="form-label" for="ativo">Produto ativo:</label>
-        <label class="form-label-radio" for="sim">Sim</label> <input checked type="radio" name="ativo" value="S">
-        <label class="form-label-radio" for="nao">Não</label> <input type="radio" name="ativo" value="N">
+        <label class="form-label" for="ativo">Produto Ativo:</label>
+        <label class="form-label-radio" for="ativo-sim">Sim</label>
+        <input checked type="radio" id="ativo-sim" name="ativo" value="S">
+        <label class="form-label-radio" for="ativo-nao">Não</label>
+        <input type="radio" id="ativo-nao" name="ativo" value="N">
     </div>
+
     <div>
         <button class="form-button" type="submit">Cadastrar</button>
     </div>
-</form>/
+</form>
